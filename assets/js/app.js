9e5ec7a2f777264d4b97053be164f10fd1795745
santosh sanap <santoshsanap143145@gmail.com>
2024-12-11 23:52:33 +0530

    small change

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,12 @@ const allAnimals = [...document.querySelectorAll(".all")];
 onColorChange = (eve) => {
   let SelectedClassName = eve.target.value;
 
-  cl(SelectedClassName);
 
   allAnimals.forEach((animal) => {
     animal.classList.add("d-none");
   });
 
   let selectedAnimals = [...document.querySelectorAll("." + SelectedClassName)];
-  // cl(SelectedDivs);
 
   selectedAnimals.forEach((animal) => {
     animal.classList.remove("d-none");
